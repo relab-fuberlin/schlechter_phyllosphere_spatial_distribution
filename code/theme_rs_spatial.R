6af8839d97c252ead32e7de93210b4e7f653129f
roschlec <roschlec@gmail.com>
2023-09-14 17:57:09 +0200

    Update codes

--- code/theme_rs_spatial.R
+++ code/theme_rs_spatial.R
@@ -1,16 +1,18 @@
 #     Ggplot theme
 theme_rs <- function() {
-  theme_bw(base_size = 12) %+replace%
+  theme_bw() %+replace%
     theme(
-      strip.background = element_rect(fill = "#f2f2f2", color = "gray", size = 0.25),
-      axis.ticks = element_line(size = 0.2),
-      axis.ticks.length=unit(.25, "cm"),
+      strip.background = element_rect(fill = NA, color = "gray", size = 0.5),
       legend.box.spacing = unit(0.1, "line"),
       legend.key.size = unit(0.1, "cm"),
       panel.background = element_blank(),
-      panel.border = element_rect(size = 0.25, fill = NA, color = "black"),
+      panel.border = element_rect(size = 1, fill = NA, color = "black"),
       panel.spacing = grid::unit(0.5, "line"),
-      panel.grid.major = element_line(size=0.25, color = "#f2f2f2"),
-      panel.grid.minor = element_blank()
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      text = element_text(color = "black"),
+      axis.ticks = element_blank(),
+      axis.title = element_text(size = 14),
+      axis.text = element_text(size = 12)
     )
 }
