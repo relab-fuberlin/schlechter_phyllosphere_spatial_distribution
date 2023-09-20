ddf478e6017e185bea2e94dfccf4030644196975
roschlec <roschlec@gmail.com>
2023-09-21 01:03:14 +0200

    Upload coordinate data and data processing code

---
+++ code/coordinates.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+# Data processing cell coordinates
+source("code/libraries_syncom.R")
+
+metadata = read_csv(here('data', 'comm_id.csv'))
+data.S2 = read.csv(here('data', 'syn2_coord_proc.csv'), header = TRUE, row.names = "X")
+data.S3 = read.csv(here('data', 'syn3_coord_proc.csv'), header = TRUE, row.names = "X")
+data.all = rbind(data.S2, data.S3)
+
+metadata %>% print(n=232)
+
+data.all %>%
+    mutate(synID = case_when(
+        syn == "s2" & comm == "comm01" ~ "C",
+        syn == "s2" & comm == "comm04" ~ "C",
+        syn == "s2" & comm == "comm07" ~ "C",
+        syn == "s2" & comm == "comm10" ~ "C",
+        syn == "s2" & comm == "comm13" ~ "C",
+        TRUE ~ syn),
+        synID = str_replace(synID, "s", "S"),
+        dpi = str_replace(dpi, "7d", "07dpi"),
+        dpi = str_replace(dpi, "14d", "14dpi"),
+        comID = str_replace(comm, "comm", "Com"),
+        comm_id = str_replace(comID, "Com", ""),
+        syncom = paste(synID, comm_id, sep=".")) %>%
+    rename(channel = ch) %>% 
+    left_join(., metadata, by = c("exp", "dpi", "synID", "comID", "syncom", "channel")) %>% 
+    select(exp, dpi, synID, comID, syncom, strain, channel, img, obj_n, area, x, y) %>% 
+    saveRDS(file = here('results', 'coordenates.rds'))
