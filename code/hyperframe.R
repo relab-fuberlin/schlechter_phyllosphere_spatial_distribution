146216305cafe2f44eb083fba53b18a40976c626
roschlec <roschlec@gmail.com>
2023-09-26 09:53:50 +0200

    Update codes

--- code/hyperframe.R
+++ code/hyperframe.R
@@ -2,25 +2,38 @@
 
 library(spatstat)
 
-# Set observation window in a two-dimensional plane
+# Set variables for creating the hyperframe in a two-dimensional plane
 xrange <- c(0,124.94)
 yrange <- c(0,100.24)
 unit <- "micron"
 W <- owin(xrange, yrange, unitname=unit)
+formula_syncom <- "~syncom + dpi + exp + img"
+formula_strain <- "~syncom + dpi + exp + img + strain"
  
 # Create hyperframe with marked points for channels
 coordinates <- readRDS(here('results', 'coordinates.rds'))
 
-clist <- split(coordinates, ~syncom+dpi+exp+img, sep="_")   # split by replicate
-clist <- lapply(clist, na.omit)                             # removes NA in data frames
-clist <- clist[sapply(clist, function(x) dim(x)[1]) > 0]    # removes empty data frames
-clist <- lapply(clist, dplyr::select, c(x, y, channel))     # moves the x,y coordinates to the beginning
+hyperframe_function <- function(df, xrange, yrange, unit, formula){
+    #
+    # df: dataframe containing coordinates and metadata
+    # xrange, yrange: each a vector of two values c(x0,x1) and c(y0,y1)
+    # unit: str character indicating the unit of the two-dimensional plane
+    # formula: str chr of the form "~var1+var2+varn" indicating the grouping factors
+    #
+    clist <- split(df, as.formula(formula), sep="_")
+    clist <- lapply(clist, na.omit)
+    clist <- clist[sapply(clist, function(x) dim(x)[1]) > 0]
+    clist <- lapply(clist, dplyr::select, c(x, y, channel))
+    list_coord <- lapply(clist, "[", c("x", "y"))
+    wlist <- rep(list(W), length(list_coord))
+    plist <- mapply(as.ppp, clist, W = wlist, SIMPLIFY = FALSE, multitype=TRUE)
+    H <- hyperframe(coord = plist, rep = names(plist))
+    
+    return(H)
+}
 
-list_coord <- lapply(clist, "[", c("x", "y"))
-wlist <- rep(list(W), length(list_coord))
-plist <- mapply(as.ppp, clist, W = wlist, SIMPLIFY = FALSE, multitype=TRUE)
-
-H <- hyperframe(coord = plist, rep = names(plist))
+H_syncom <- hyperframe_function(coordinates, xrange, yrange, unit, formula_syncom)
+H_strain <- hyperframe_function(coordinates, xrange, yrange, unit, formula_strain)
 
 summary_data <- coordinates %>% 
     group_by(syncom, dpi, exp, img, channel) %>% 
--- code/spat_K.R
+++ code/spat_K.R
@@ -1,4 +1,8 @@
 
+
+# Set seed
+set.seed(19900725)
+
 # Load data
 H <- readRDS(here('results', 'hyperframe.rds'))
 H_summary <- readRDS(here('results', 'hyperframe_summary.rds'))
@@ -12,3 +16,21 @@ H_summary %>%
     tally()
 
 hist(with(H, npoints(coord)))
+
+K_inhom <- with(H, envelope(coord, Kinhom, correction ="Ripley", r=seq(0,30,0.2)))
+saveRDS(K_inhom, here('results', 'stat_K_inhom.rds'))
+
+# Create data frame
+datalist = list()
+for (i in 1:nrow(H)){
+    datalist[[i]] <- data.frame(
+        syncom = summary_data$syncom[i],
+        dpi = summary_data$dpi[i],
+        rep = summary_data$img[i],
+        r = K_inhom[[i]]$r,
+        obs = K_inhom[[i]]$obs,
+        lo = K_inhom[[i]]$lo,
+        hi = K_inhom[[i]]$hi)
+}
+K_df = data.table::rbindlist(datalist) %>% na.omit
+
