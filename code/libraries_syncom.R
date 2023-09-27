d77f5652b508ee3943d71233c246bf25b400aeb8
roschlec <roschlec@gmail.com>
2023-09-27 17:58:44 +0200

    Update codes

--- code/libraries_syncom.R
+++ code/libraries_syncom.R
@@ -10,19 +10,12 @@ library(reshape2)
 
 # plots
 library(patchwork)
-library(gridExtra)
 library(RColorBrewer)
-#library(ggpubr)
+library(ggpubr)
 library(wesanderson)
 library(ggdist)
 
 # stats
 library(car)
-library(emmeans)
-library(lsr)
 library(spatstat)
-library(MESS)
-library(MASS)
-library(betareg)
-library(multcomp)
-library(multcompView)
+library(DescTools)
