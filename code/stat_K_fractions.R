942344252a623a59ed9cb80776f3f818e81d2789
roschlec <roschlec@gmail.com>
2023-09-27 10:11:14 +0200

    Add code spatial fractions

---
+++ code/stat_K_fractions.R
@@ -0,0 +1,30 @@
+## Spatial fractions
+
+#   Open file
+data <- readRDS(here('results', 'stat_K_inhom_table.rds'))
+
+data %>%
+    mutate(
+        aggregate = case_when(obs > hi ~ 1, TRUE ~ 0),
+        regular = case_when(obs < lo ~ 1, TRUE ~ 0),
+        random = case_when(obs < hi & obs > lo ~ 1, TRUE ~ 0)) %>% 
+    group_by(strain, dpi, r) %>% 
+    summarise(
+        aggregate = sum(aggregate),
+        regular = sum(regular),
+        random = sum(random),
+        .groups = 'drop') %>% 
+    mutate(
+        total = aggregate + regular + random,
+        aggregate_fraction = if_else(is.na(aggregate/total), 0, aggregate/total),
+        regular_fraction = if_else(is.na(regular/total), 0, regular/total),
+        random_fraction = if_else(is.na(random/total), 0, random/total)) %>% 
+    pivot_longer(cols = c("aggregate_fraction", "regular_fraction", "random_fraction"), 
+                 names_to = "type", 
+                 values_to = "fraction", 
+                 values_drop_na = TRUE) %>% 
+    select(strain, dpi, r, type, fraction) %>% 
+    mutate(strain = factor(strain),
+           dpi = factor(dpi),
+           type = factor(dpi, levels = c('regular_fraction', 'random_fraction', 'aggregate_fraction'))) %>% 
+    write.csv(here('results', 'stat_K_fractions.csv'))
