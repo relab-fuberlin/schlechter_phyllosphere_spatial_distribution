33ad5ade6f01bbe2fdbf4833e841dbc9fec5b9d1
roschlec <roschlec@gmail.com>
2023-10-04 16:54:39 +0200

    Update codes section 3

--- code/spat_K_distance.R
+++ code/spat_K_distance.R
@@ -28,11 +28,13 @@ ecdf_all <- data %>%
 r_c <- ecdf_all %>% 
     filter(synID == "C") %>% 
     select(-syncom:-com)
+
 r_fold <- ecdf_all %>% 
     filter(synID != "C") %>% 
-    left_join(., r_c, by=c("dpi", "strain"), suffix = c(".inter", ".intra")) %>% 
+    left_join(., r_c, by=c("dpi", "strain", "taxa"), suffix = c(".inter", ".intra")) %>% 
     mutate(fold = r.inter/r.intra,
            log2FC = log2(fold))
+
 r_fold_s2 <- r_fold %>% 
     filter(synID=="S2") %>% 
     select(syncom, dpi, strain, r.inter, log2FC) %>% 
@@ -41,52 +43,15 @@ r_fold_s2 <- r_fold %>%
     arrange(to) %>% 
     pivot_wider(id_cols = c(strain, dpi), names_from=to, values_from=c(r.inter,log2FC)) %>% 
     arrange(strain)
+
 r_fold_s3 <- r_fold %>% 
     filter(synID=="S3") %>% 
     pivot_wider(id_cols = c(strain, dpi), names_from=syncom, values_from=c(r.inter,log2FC)) %>% 
     arrange(strain)
+
 r_fold_back <- cbind(r_fold_s2, r_fold_s3[,c(-1:-2)]) %>% 
     pivot_longer(cols=-c(strain,dpi), names_to = c("type", "strain2"), names_sep="_", ) %>% 
     pivot_wider(id_cols = c(strain, dpi, strain2), names_from = type, values_from = value) %>% 
     ungroup %>% 
     arrange(dpi,strain) %>% 
     mutate(index = seq(1, nrow(.)))
-    
-
-r_fold_back %>% 
-    ggplot(aes(x=fct_reorder(strain2, index), y=strain))+
-    facet_wrap(~dpi, ncol=1)+
-    geom_tile(colour='black', fill='white')+
-    geom_point(aes(fill=log2FC, size = r.inter), shape = 21)+
-    scale_fill_gradientn(colours = wes_palette("Zissou1")[c(1,1,3,5,5)], values = c(0,0.5,1), limits=c(-0.4,0.4))+
-    coord_fixed()+
-    scale_size_continuous(range = c(2,12), limits=c(8,15))
-
-ecdf_all %>% 
-    ggplot(aes(x=strain, y=r))+
-    geom_jitter(width=0.1)+
-    geom_boxplot(alpha = 0.5, width=0.2, outlier.alpha = 0)+
-    scale_y_continuous(limits = c(7,16), breaks = seq(8,16,4), expand = c(0,0))+
-    theme_classic()
-
-#dpi
-ecdf_all %>% 
-    wilcox_test(r ~ dpi)
-
-#syncom
-ecdf_all %>% 
-    group_by(dpi) %>% 
-    dunn_test(r ~ synID, p.adjust.method='holm')
-
-#taxa
-ecdf_all %>% 
-    group_by(dpi) %>% 
-    wilcox_test(r ~ taxa)
-
-#strain
-ecdf_all %>% 
-    kruskal_test(r ~ strain)
-
-ecdf_all %>% 
-    group_by(dpi) %>% 
-    dunn_test(r ~ strain, p.adjust.method='holm')
