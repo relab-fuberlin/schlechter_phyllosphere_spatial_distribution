414d72aaafeaf49678484390ddb7228a51020e50
roschlec <roschlec@gmail.com>
2023-10-06 16:09:54 +0200

    Update code

--- code/spat_pcf.R
+++ code/spat_pcf.R
@@ -17,3 +17,8 @@ H_S3 <- H[H$C0 > 10 & H$C1 >10 & H$C2>10,]
 pcf_S3_C0C1 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 pcf_S3_C0C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 pcf_S3_C1C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C1", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
+
+write_rds(pcf_S2)
+write_rds(pcf_S3_C0C1)
+write_rds(pcf_S3_C0C2)
+write_rds(pcf_S3_C1C2)
