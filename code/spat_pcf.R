5ddccd953f14bd408e0525739f6d7d36f60c2a1b
roschlec <roschlec@gmail.com>
2023-10-08 18:47:56 +0200

    Update pcf code

--- code/spat_pcf.R
+++ code/spat_pcf.R
@@ -1,23 +1,30 @@
 ##  Pair correlation analysis
 library(spatstat)
 
+set.seed(19900725)
+
 #   Load data
 H <- readRDS(here('results', 'hyperframe_syncom.rds'))
 
 #   PCF for S2
-set.seed(20230810)
 H_S2 <- H[grep("S2", H$syncom)]
 H_S2 <- H_S2[H_S2$C0 > 10 & H_S2$C1 > 10]
-pcf_S2 <- with(H_S2, envelope(coord, pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
+pcf_S2 = list(NULL)
+for(i in 1:nrow(H_S2)){
+    tryCatch({
+        pcf_S2[[i]] <- envelope(H_S2$coord[[i]], pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0,30,0.2))
+        print(paste("Analysing: ", i, H_S2$syncom[[i]]))
+    }, error=function(e){cat("ERROR :", conditionMessage(e), "\n")})
+    
+}
 
 #   PCF for S3
-set.seed(19900725)
 H_S3 <- H[H$C0 > 10 & H$C1 >10 & H$C2>10,]
 pcf_S3_C0C1 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 pcf_S3_C0C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 pcf_S3_C1C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C1", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 
-write_rds(pcf_S2, here('results', 'pcf_S2.rds'))
+write_rds(pcf_S3_C0C1, here('results', 'pcf_S2.rds'))
 write_rds(pcf_S3_C0C1, here('results', 'pcf_S3_C0C1.rds'))
 write_rds(pcf_S3_C0C2, here('results', 'pcf_S3_C0C2.rds'))
 write_rds(pcf_S3_C1C2, here('results', 'pcf_S3_C1C2.rds'))
