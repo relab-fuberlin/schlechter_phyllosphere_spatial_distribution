27a3dd8ca652100af92785891c25ed88fb47106a
roschlec <roschlec@gmail.com>
2023-10-13 15:40:48 +0200

    Optimise code

--- code/spat_pcf.R
+++ code/spat_pcf.R
@@ -1,24 +1,22 @@
 ##  Pair correlation analysis
-library(spatstat)
 
+#   Set seed
 set.seed(19900725)
 
-#   Load data
-H <- readRDS(here('results', 'hyperframe_syncom.rds'))
+#   Dependencies
+library(spatstat)
+library(tidyverse)
+source(here('code', 'function_pcf.R')) # Custom PCF function
 
-pcf_function <- function(hf, i, j){
-    safe_envelope = possibly(.f = envelope, otherwise=NULL)
-    pcf <- map(hf$coord, safe_envelope, pcfcross.inhom, i=i, j=j, divisor = "d", correction = "isotropic", r=seq(0,30,0.2))
-    pcf_tibble <- map(pcf, tibble) %>% 
-        enframe(name = "rep") %>%
-        unnest(cols = "value")
-    
-    return(pcf_tibble)
-}
+#   Data
+H <- readRDS(here('results', 'hyperframe_syncom.rds')) # Read hyperframe
 
 #   PCF for S2
-H_S2 <- H[grep("S2", H$syncom)]
-H_S2 <- H_S2[H_S2$C0 > 10 & H_S2$C1 > 10]
+H_S2 <- H[grep("S2", H$syncom)] # Filter by S2 communities. C and S3 are excluded
+H_S2 <- H_S2[H_S2$C0 > 10 & H_S2$C1 > 10] # Filter by FOV with at least 10 cells per channel
+
+pcf_S2_C0C1 <- pcf_function(H_S2, i = "C0", j = "C1")
+
 pcf_S2 = list(NULL)
 for(i in 1:nrow(H_S2)){
     tryCatch({
@@ -32,16 +30,21 @@ for(i in 1:nrow(H_S2)){
 }
 
 
-pcf_function(H_S2, i = "C0", j = "C1")
-
 
 #   PCF for S3
-H_S3 <- H[H$C0 > 10 & H$C1 >10 & H$C2>10,]
+H_S3 <- H[H$C0 > 10 & H$C1 >10 & H$C2>10,] # Filter by S3 and FOV with at least 10 cells per channel
+pcf_S3_C0C1 <- pcf_function(H_S3, i = "C0", j = "C1")
+pcf_S3_C0C2 <- pcf_function(H_S3, i = "C0", j = "C2")
+pcf_S3_C1C2 <- pcf_function(H_S3, i = "C1", j = "C2")
+
+
 pcf_S3_C0C1 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0.2,30,0.2)))
 pcf_S3_C0C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 pcf_S3_C1C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C1", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
 
-write_rds(pcf_S2, here('results', 'pcf_S2.rds'))
-write_rds(pcf_S3_C0C1, here('results', 'pcf_S3_C0C1.rds'))
-write_rds(pcf_S3_C0C2, here('results', 'pcf_S3_C0C2.rds'))
-write_rds(pcf_S3_C1C2, here('results', 'pcf_S3_C1C2.rds'))
+
+#   Combine results of PCF for S2 and S3 communities
+pcf <- rbind(pcf_S2_C0C1, pcf_S3_C0C1, pcf_S3_C0C2, pcf_S3_C1C2)
+
+#   Save data as RDS
+write_rds(pcf, here('results', 'pcf.rds'))
