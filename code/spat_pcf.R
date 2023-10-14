81f6db0975380428e88813e1bb6d38d9fd9166f3
roschlec <roschlec@gmail.com>
2023-10-14 10:22:06 +0200

    Add metadata to the PCF table

--- code/spat_pcf.R
+++ code/spat_pcf.R
@@ -4,6 +4,7 @@
 set.seed(19900725)
 
 #   Dependencies
+library(here)
 library(spatstat)
 library(tidyverse)
 source(here('code', 'function_pcf.R')) # Custom PCF function
@@ -15,22 +16,46 @@ H <- readRDS(here('results', 'hyperframe_syncom.rds')) # Read hyperframe
 H_S2 <- H[grep("S2", H$syncom)] # Filter by S2 communities. C and S3 are excluded
 H_S2 <- H_S2[H_S2$C0 > 10 & H_S2$C1 > 10] # Filter by FOV with at least 10 cells per channel
 pcf_S2_C0C1 <- pcf_function(H_S2, i = "C0", j = "C1")
-pcf_S2_C0C1 <- pcf_S2_C0C1 %>% mutate(pair = "C0.C1")
+pcf_S2_C0C1 <- pcf_S2_C0C1 %>% mutate(pair = "C0.C1", pair_inv = "C1.C0")
 
 #   PCF for S3
 H_S3 <- H[H$C0 > 10 & H$C1 >10 & H$C2>10,] # Filter by S3 and FOV with at least 10 cells per channel
 pcf_S3_C0C1 <- pcf_function(H_S3, i = "C0", j = "C1")
-pcf_S3_C0C1 <- pcf_S3_C0C1 %>% mutate(pair = "C0.C1")
+pcf_S3_C0C1 <- pcf_S3_C0C1 %>% mutate(pair = "C0.C1", pair_inv = "C1.C0")
 
 pcf_S3_C0C2 <- pcf_function(H_S3, i = "C0", j = "C2")
-pcf_S3_C0C2 <- pcf_S3_C0C2 %>% mutate(pair = "C0.C2")
+pcf_S3_C0C2 <- pcf_S3_C0C2 %>% mutate(pair = "C0.C2", pair_inv = "C2.C0")
 
 pcf_S3_C1C2 <- pcf_function(H_S3, i = "C1", j = "C2")
-pcf_S3_C1C2 <- pcf_S3_C1C2 %>% mutate(pair = "C1.C2")
+pcf_S3_C1C2 <- pcf_S3_C1C2 %>% mutate(pair = "C1.C2", pair_inv = "C2.C1")
 
 
 #   Combine results of PCF for S2 and S3 communities
-pcf <- rbind(pcf_S2_C0C1, pcf_S3_C0C1, pcf_S3_C0C2, pcf_S3_C1C2)
+pcf <- rbind(pcf_S2_C0C1, pcf_S3_C0C1, pcf_S3_C0C2, pcf_S3_C1C2) %>% 
+    pivot_longer(pair:pair_inv, names_to="type_pair", values_to="pair") %>% 
+    separate(rep, into=c("syncom", "dpi", "exp", "img"), sep="_")
+
+#   Add metadata
+metadata <- read.csv(here('data', 'comm_id.csv')) %>% 
+    group_by(exp, dpi, syncom, strain, channel) %>%
+    unique() %>% 
+    pivot_wider(id_cols = exp:syncom, names_from=channel, values_from = strain) %>% 
+    unite("C0.C1", C0, C1, sep = ".", remove = FALSE) %>% 
+    unite("C1.C0", C1, C0, sep = ".", remove = FALSE) %>% 
+    unite("C0.C2", C0, C2, sep = ".", remove = FALSE) %>% 
+    unite("C2.C0", C2, C0, sep = ".", remove = FALSE) %>% 
+    unite("C1.C2", C1, C2, sep = ".", remove = FALSE) %>% 
+    unite("C2.C1", C2, C1, sep = ".", remove = TRUE) %>% 
+    select(-C0) %>% 
+    pivot_longer(cols="C0.C1":"C2.C1", names_to = 'pair', values_to = "strain_pair") %>% 
+    filter(!grepl("NA", strain_pair))
+
+unique_pair <- c("meL85.smfr1", "meL85.spfa2", "meL85.meL92", "meL85.mr01", "meL92.smfr1", "meL92.spfa2", "meL92.mr01", "mr01.smr1", "mr01.spfa2", "smfr1.spfa2")
+
+stat_pcf <- left_join(pcf, metadata, by=c("exp", "dpi", "syncom", "pair")) %>% 
+    select(syncom, synID, comID, dpi, exp, img, r, obs, theo, lo, hi, strain_pair) %>% 
+    filter(strain_pair %in% unique_pair)
 
 #   Save data as RDS
-write_rds(pcf, here('results', 'pcf.rds'))
+write_rds(stat_pcf, here('results', 'stat_pcf_inhom_table.rds'))
+
