84debe84617aa967d716932ff4e6e97f78ec47bc
roschlec <roschlec@gmail.com>
2023-10-14 08:39:41 +0200

    Optimise code

--- code/spat_pcf.R
+++ code/spat_pcf.R
@@ -14,33 +14,19 @@ H <- readRDS(here('results', 'hyperframe_syncom.rds')) # Read hyperframe
 #   PCF for S2
 H_S2 <- H[grep("S2", H$syncom)] # Filter by S2 communities. C and S3 are excluded
 H_S2 <- H_S2[H_S2$C0 > 10 & H_S2$C1 > 10] # Filter by FOV with at least 10 cells per channel
-
 pcf_S2_C0C1 <- pcf_function(H_S2, i = "C0", j = "C1")
-
-pcf_S2 = list(NULL)
-for(i in 1:nrow(H_S2)){
-    tryCatch({
-        print(paste("Analysing: ", i, H_S2$syncom[[i]]))
-        pcf_S2[[i]] <- envelope(H_S2$coord[[i]], pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0,30,0.2))
-        pcf_S2[[i]] <- pcf_S2[[i]] %>% 
-            tibble %>% 
-            mutate(pair = "c0c1") %>% 
-            cbind(., H_S2[i,-1], row.names=NULL)
-    }, error=function(e){cat("ERROR :", conditionMessage(e), "\n")})
-}
-
-
+pcf_S2_C0C1 <- pcf_S2_C0C1 %>% mutate(pair = "C0.C1")
 
 #   PCF for S3
 H_S3 <- H[H$C0 > 10 & H$C1 >10 & H$C2>10,] # Filter by S3 and FOV with at least 10 cells per channel
 pcf_S3_C0C1 <- pcf_function(H_S3, i = "C0", j = "C1")
+pcf_S3_C0C1 <- pcf_S3_C0C1 %>% mutate(pair = "C0.C1")
+
 pcf_S3_C0C2 <- pcf_function(H_S3, i = "C0", j = "C2")
+pcf_S3_C0C2 <- pcf_S3_C0C2 %>% mutate(pair = "C0.C2")
+
 pcf_S3_C1C2 <- pcf_function(H_S3, i = "C1", j = "C2")
-
-
-pcf_S3_C0C1 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C1", divisor = "d", correction = "isotropic", r=seq(0.2,30,0.2)))
-pcf_S3_C0C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C0", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
-pcf_S3_C1C2 <- with(H_S3, envelope(coord, pcfcross.inhom, i = "C1", j = "C2", divisor = "d", correction = "isotropic", r=seq(0,30,0.2)))
+pcf_S3_C1C2 <- pcf_S3_C1C2 %>% mutate(pair = "C1.C2")
 
 
 #   Combine results of PCF for S2 and S3 communities
