d12e7cf0ffedd3c7291d00fe583790132d444327
roschlec <roschlec@gmail.com>
2023-11-07 07:47:44 +0100

    Add download bash script linkned to zenodo

--- code/bash_download.sh
+++ code/bash_download.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Download datasets
-wget -P data/ "URL"
-tar -xvz *.tar.gz
-rm *.tar.gz
+echo "Check dependency"
+
+if type zenodo_get &>/dev/null; then 
+    echo "zenodo_get found"
+    mkdir data
+    zenodo_get 10.5281/zenodo.10036117 -o ./data
+
+else
+    echo "zenodo_get not found"
+fi
