a0661b947c2d9de820a0991d16ea112174ffe593
roschlec <roschlec@gmail.com>
2024-10-30 17:17:33 +0100

    Create directory for revised figures

--- code/bash_resubmission.sh
+++ code/bash_resubmission.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#   Create an output directory
+mkdir -p results
+
 #   Pipeline
 Rscript -e 'library(rmarkdown); rmarkdown::render("docs/05resubmission.Rmd", "html_document")'
 Rscript -e 'library(rmarkdown); rmarkdown::render("docs/06resubmission.Rmd", "html_document")'
