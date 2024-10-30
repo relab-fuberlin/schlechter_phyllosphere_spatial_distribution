b363f1c4cbc91295a3e79b3a50d6698947a20bdd
roschlec <roschlec@gmail.com>
2024-10-30 16:25:02 +0100

    Change name to a few variables

--- code/spat_K_fractions_auc.R
+++ code/spat_K_fractions_auc.R
@@ -7,27 +7,26 @@ library(tidyverse)
 library(DescTools)
 
 # Load data
-fractions <- read.csv(here('results', 'stat_K_fractions.csv'), header = TRUE, row.names = "X") %>% 
+K_fractions <- read.csv(here('results', 'stat_K_fractions.csv'), header = TRUE, row.names = "X") %>% 
     tibble %>% 
     separate(col='syncom', into = c('synID', 'synC'), sep = '\\.', remove = FALSE) %>% 
-    select(-synC) %>% 
+    dplyr::select(-synC) %>% 
     mutate(taxa = case_when(
         strain == "meL85" ~ "Methylobacterium",
         strain == "meL92" ~ "Methylobacterium",
         strain == "mr01" ~ "Methylobacterium",
         TRUE ~ "Sphingomonas"))
 
-auc_fractions <- fractions %>% 
+K_auc_fractions <- K_fractions %>% 
     group_by(syncom, synID, taxa, dpi, type, strain) %>% 
     summarise(auc = AUC(r, fraction, absolutearea = FALSE),
               .groups = 'drop')
 
-auc_C <- auc_fractions %>% 
+K_auc_C <- K_auc_fractions %>% 
     filter(synID == 'C') %>% 
-    select(-syncom:-taxa)
+    dplyr::select(-syncom:-taxa)
 
-auc_fold_change <- auc_fractions %>% 
+K_auc_fold_change <- K_auc_fractions %>% 
     filter(synID != 'C') %>% 
-    inner_join(., auc_C, by = c('dpi', 'type', 'strain'), suffix = c('.inter', '.intra')) %>% 
+    inner_join(., K_auc_C, by = c('dpi', 'type', 'strain'), suffix = c('.inter', '.intra')) %>% 
     mutate(fractional_change = (auc.inter-auc.intra)/auc.intra)
-
